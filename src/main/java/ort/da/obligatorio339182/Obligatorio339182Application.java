002f0827ae32cb458cc0b29a306620342f1323c3
339182 <FA339182@fi365.ort.edu.uy>
2025-10-04 15:03:26 -0300

    First commit

---
+++ src/main/java/ort/da/obligatorio339182/Obligatorio339182Application.java
@@ -0,0 +1,13 @@
+package ort.da.obligatorio339182;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obligatorio339182Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Obligatorio339182Application.class, args);
+	}
+
+}
---
+++ src/test/java/ort/da/obligatorio339182/Obligatorio339182ApplicationTests.java
@@ -0,0 +1,13 @@
+package ort.da.obligatorio339182;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obligatorio339182ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
